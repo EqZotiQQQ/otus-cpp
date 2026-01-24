f62c6eebdb5886794a09246ee01bff5674777341
Mikhail Fedyakov <fedyakovmikael@gmail.com>
2026-01-24 12:43:56 +0000

    added multiindex getter

--- otus_hws/hw6/include/matrix.hpp
+++ otus_hws/hw6/include/matrix.hpp
@@ -18,6 +18,7 @@ struct Cell {
     int value;
 };
 
+
 struct by_x {};
 struct by_y {};
 struct by_xy {};
@@ -35,13 +36,51 @@ using Matrix = multi_index_container<
 class MatrixInterface {
 public:    
     virtual void insert(int x, int y, int value) = 0;
-    virtual ~MatrixInterface() = 0;
+    virtual int get_value_at(int x, int y) const = 0;
+    virtual ~MatrixInterface() {};
 private:
 };
 
+class CellProxy {
+public:
+    CellProxy(MatrixInterface& matrix, int x, int y)
+        : matrix_(matrix), x_(x), y_(y) {}
+
+    operator int() const {
+        return matrix_.get_value_at(x_, y_);
+    }
+
+    CellProxy& operator=(int value) {
+        matrix_.insert(x_, y_, value);
+        return *this;
+    }
+
+private:
+    MatrixInterface& matrix_;
+    int x_;
+    int y_;
+};
+
+class RowProxy {
+public:
+    RowProxy(MatrixInterface& matrix, int x)
+        : matrix_(matrix), x_(x) {}
+
+    CellProxy operator[](int y) {
+        return CellProxy(matrix_, x_, y);
+    }
+
+private:
+    MatrixInterface& matrix_;
+    int x_;
+};
+    
+
 class MatrixImpl: public MatrixInterface {
 public:
-    MatrixImpl(int default_value): default_value_(default_value) {}
+    MatrixImpl(int default_value): default_value_(default_value) {
+        spdlog::info("Created matrix with default value {}", default_value);
+    }
 
     void insert(int x, int y, int value) override {
         auto& index = matrix_.get<by_xy>();
@@ -62,6 +101,13 @@ public:
             }
         }
     }
+
+    int get_value_at(int x, int y) const override {
+        auto& index = matrix_.get<by_xy>();
+        auto it = index.find(std::make_tuple(x, y));
+        return it == index.end() ? default_value_ : it->value;
+    }
+
 private:
     Matrix matrix_;
     int default_value_;
@@ -72,9 +118,27 @@ public:
     MatrixProxy(int default_value): matrix_impl_(default_value) {}
 
     void insert(int x, int y, int value) override {
-        spdlog::info("Trying to add to {}:{} value {}", x, y, value);
+        spdlog::info("Trying to add to [{}:{}] value {}", x, y, value);
         matrix_impl_.insert(x, y, value);
     }
+
+    int get_value_at(int x, int y) const override {
+        return matrix_impl_.get_value_at(x, y);
+    }
+
+    RowProxy operator[](int x) {
+        return RowProxy(*this, x);
+    };
 private:
     MatrixImpl matrix_impl_;
+};
+
+template <>
+struct fmt::formatter<CellProxy> : fmt::formatter<int> {
+    template <typename FormatContext>
+    auto format(const CellProxy& cell, FormatContext& ctx) const {
+        return fmt::formatter<int>::format(
+            static_cast<int>(cell), ctx
+        );
+    }
 };
--- otus_hws/hw6/src/main.cpp
+++ otus_hws/hw6/src/main.cpp
@@ -11,6 +11,17 @@
 #include "matrix.hpp"
 
 int main() {
-    std::cout << "hi" << std::endl;
+    MatrixProxy matrix{-1};
+    matrix.insert(4, 2, 5);
+    matrix.insert(4, 24, 53);
+
+    spdlog::info("Get cell: {} {} -> {}", 4, 2, matrix[4][2]);
+    spdlog::info("Get cell: {} {} -> {}", 4, 1, matrix[4][1]);
+    spdlog::info("Get cell: {} {} -> {}", 5, 5, matrix[5][5]);
+    spdlog::info("Get cell: {} {} -> {}", 4, 24, matrix[4][24]);
+    
+    matrix.insert(4, 24, 553);
+    spdlog::info("Get cell: {} {} -> {}", 4, 24, matrix[4][24]);
+
     return 0;
 }
