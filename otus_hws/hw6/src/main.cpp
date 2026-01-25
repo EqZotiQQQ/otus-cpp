caaee3e86464048a6a1f766757ed1ac0b31c6225
Mikhail Fedyakov <fedyakovmikael@gmail.com>
2026-01-25 14:14:11 +0000

    t

--- otus_hws/hw6/src/main.cpp
+++ otus_hws/hw6/src/main.cpp
@@ -12,7 +12,7 @@
 
 int main() {
     {    
-        MatrixProxy matrix{-1};
+        MatrixProxy<int, -1> matrix;
         
         spdlog::info("Matrix size at the start: {}", matrix.size());
 
@@ -40,7 +40,7 @@ int main() {
         assert(matrix.size() == 2);
     }
     {
-        MatrixProxy matrix{0};
+        MatrixProxy<int, 0> matrix;
         for (int i = 0; i < 10; i++) {
             int reverse_value = 9 - i;
             matrix[i][i] = i;
